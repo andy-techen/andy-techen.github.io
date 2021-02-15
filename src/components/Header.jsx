58d7e7228b710094070acb0d8c5689ed24a1ab8a
Andy <andy27479@gmail.com>
2021-02-15 15:04:00 +0800

    adjusted gitignore

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Layout,Navigation } from "react-mdl";
+
+function Header() {
+  return (
+    <Layout>
+      <Navigation>
+        <a href="/">Link</a>
+        <a href="/">Link</a>
+        <a href="/">Link</a>
+        <a href="/">Link</a>
+      </Navigation>
+    </Layout>
+  );
+}
+
+export default Header;
