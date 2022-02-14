3c75525a0018a59876a2a43fd6bb48c4aa83f3a2
Andy Chen <andy27479@gmail.com>
2022-02-13 20:04:06 -0500

    added br between header

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,18 @@ export default function IndexPage() {
     <>
       <Seo title="Andy Chen's Portfolio" />
       <Page useSplashScreenAnimation>
+        <br></br>
+        <br></br>
         <HeroSection sectionId="hero" />
+        <br></br>
+        <br></br>
         {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
         <AboutSection sectionId="about" heading="About Me" />
+        <br></br>
+        <br></br>
         <InterestsSection sectionId="skills" heading="Skills" />
+        <br></br>
+        <br></br>
         <ProjectsSection sectionId="projects" heading="Latest Projects" />
         {/* <ContactSection sectionId="github" heading="Issues?" /> */}
       </Page>
