f75928271c2fab69fe7cc93c9c0dd84fa1427f21
Andy Chen <achen@schoolinks.com>
2026-04-11 10:49:15 -0400

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ThemeProvider from '@/components/ThemeProvider'
 export const metadata: Metadata = {
   title: 'Andy Chen',
   description: 'Portfolio of Andy Chen',
+  icons: { icon: '/favicon.svg' },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
